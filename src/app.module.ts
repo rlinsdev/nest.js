18f9addb7fe8dda3b2ecd8dd410b5c2dce3886ef
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-20 11:31:32 -0300

    Generate Resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TweetsModule } from './tweets/tweets.module';
 
-@Module({})
+@Module({
+  imports: [TweetsModule]
+})
 export class AppModule {}
