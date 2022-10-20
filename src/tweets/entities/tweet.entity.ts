5dec5f871cf9a67015491597fa9ff36c3d5af717
Rodrigo Lins <rlinsdev@gmail.com>
2022-10-20 17:26:51 -0300

    entities

--- src/tweets/entities/tweet.entity.ts
+++ src/tweets/entities/tweet.entity.ts
@@ -1,1 +1,24 @@
-export class Tweet {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TweetDocument = Tweet & Document;
+
+export type TweetProps = {
+  content: string;
+  screen_name: string;
+};
+
+@Schema()
+export class Tweet {
+  constructor(props: TweetProps) {
+    Object.assign(this, props);
+  }
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ required: true })
+  screen_name: string;
+}
+
+export const TweetSchema = SchemaFactory.createForClass(Tweet);
